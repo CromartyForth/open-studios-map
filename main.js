e2a283d6089d9e66e703dbb4d163f60aeee5b27f
Glenn Campbell <66366989+CromartyForth@users.noreply.github.com>
2024-02-24 19:27:54 +0000

    Update main.js

--- main.js
+++ main.js
@@ -11,12 +11,12 @@ document.addEventListener("DOMContentLoaded", function(){
         attribution: '© OpenStreetMap'
     }).addTo(map);
 
-    var Stadia_StamenTonerLabels = L.tileLayer('https://tiles.stadiamaps.com/tiles/stamen_toner_labels/{z}/{x}/{y}{r}.{ext}', {
-        minZoom: 0,
-        maxZoom: 17,
-        attribution: '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://www.stamen.com/" target="_blank">Stamen Design</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-        ext: 'png'
-    }).addTo(map);
+    //var Stadia_StamenTonerLabels = L.tileLayer('https://tiles.stadiamaps.com/tiles/stamen_toner_labels/{z}/{x}/{y}{r}.{ext}', {
+    //    minZoom: 0,
+    //    maxZoom: 17,
+    //    attribution: '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://www.stamen.com/" target="_blank">Stamen Design</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    //    ext: 'png'
+    //}).addTo(map);
 
 
     var moseleyExchange = L.marker([52.445947, -1.889369]).addTo(map);
