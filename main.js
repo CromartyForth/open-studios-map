bf72b60386f0e1f2ddcff1d846d61bf1440357c5
Glenn <glenn@gcampbell.co.uk>
2024-04-24 21:43:49 +0100

    Change back to watercolor map tiles

--- main.js
+++ main.js
@@ -18,11 +18,10 @@ document.addEventListener("DOMContentLoaded", function(){
     
     // the map tiles
     
-    var Stadia_OSMBright = L.tileLayer('https://tiles.stadiamaps.com/tiles/osm_bright/{z}/{x}/{y}{r}.{ext}', {
-	    minZoom: 0,
-	    maxZoom: 20,
-	    attribution: '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-	    ext: 'png'}).addTo(map);
+    var watercolor = L.tileLayer('https://watercolormaps.collection.cooperhewitt.org/tile/watercolor/{z}/{x}/{y}.jpg', {
+        maxZoom: 17,
+        attribution: '© OpenStreetMap'
+    }).addTo(map);
     
 
     // user location not found set view to bounds
